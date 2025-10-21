f74e64a78b29952c863cb61d2c8c34cae15a5ea7
Abilash Ravi <abilashravi09@gmail.com>
2025-10-21 17:42:29 +0530

    evening_commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -103,7 +103,7 @@ function parallax() {
     })
 
     gsap.to(".gradients", {
-        opacity:1,
+        opacity: 1,
         scrollTrigger: {
             trigger: ".about_section",
             start: "top 85%",
@@ -285,87 +285,87 @@ function careerLine() {
     });
 }
 function cardsAnimation() {
-  gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cards",
-      start: "top-=50px top",
-      end: "+=2000",
-      pin: true,
-      pinSpacing: true,
-      scrub: 1,
-      markers: false,
-    },
-  });
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".cards",
+            start: "top-=50px top",
+            end: "+=2000",
+            pin: true,
+            pinSpacing: true,
+            scrub: 1,
+            markers: false,
+        },
+    });
 
-  tl
-    // Card 1
-    .addLabel("card1")
-    .to(".card1", { yPercent: 0, opacity: 1 })
+    tl
+        // Card 1
+        .addLabel("card1")
+        .to(".card1", { yPercent: 0, opacity: 1 })
 
-    // Card 2
-    .from(".card2", { yPercent: 75, opacity: 0 })
-    .addLabel("card2")
-    .to(".card1", { scale: 0.925, yPercent: -0.75, opacity: 1 }, "-=0.3")
-    .to(".card2", { yPercent: 0, opacity: 1 })
+        // Card 2
+        .from(".card2", { yPercent: 75, opacity: 0 })
+        .addLabel("card2")
+        .to(".card1", { scale: 0.925, yPercent: -0.75, opacity: 1 }, "-=0.3")
+        .to(".card2", { yPercent: 0, opacity: 1 })
 
-    // Card 3
-    .from(".card3", { yPercent: 75, opacity: 0 })
-    .addLabel("card3")
-    .to(".card2", { scale: 0.95, yPercent: -0.5, opacity: 1 }, "-=0.3")
-    .to(".card3", { yPercent: 0, opacity: 1 })
+        // Card 3
+        .from(".card3", { yPercent: 75, opacity: 0 })
+        .addLabel("card3")
+        .to(".card2", { scale: 0.95, yPercent: -0.5, opacity: 1 }, "-=0.3")
+        .to(".card3", { yPercent: 0, opacity: 1 })
 
-    // // Card 4
-    // .from(".card4", { yPercent: 75, opacity: 0 })
-    // .addLabel("card4")
-    // .to(".card3", { scale: 0.98, yPercent: -0.4, opacity: 1 }, "-=0.3")
-    // .to(".card4", { yPercent: 0, opacity: 1 })
+        // // Card 4
+        // .from(".card4", { yPercent: 75, opacity: 0 })
+        // .addLabel("card4")
+        // .to(".card3", { scale: 0.98, yPercent: -0.4, opacity: 1 }, "-=0.3")
+        // .to(".card4", { yPercent: 0, opacity: 1 })
 
-    // Scale down previous cards gradually
-    .to(".card1", { scale: 0.925, yPercent: -1.5, opacity: 0.9 }, "-=0.3")
-    .to(".card2", { scale: 0.95, yPercent: -1.125, opacity: 0.9 }, "-=0.3")
+        // Scale down previous cards gradually
+        .to(".card1", { scale: 0.925, yPercent: -1.5, opacity: 0.9 }, "-=0.3")
+        .to(".card2", { scale: 0.95, yPercent: -1.125, opacity: 0.9 }, "-=0.3")
     // .to(".card3", { scale: 0.98, yPercent: -0.85, opacity: 0.9 }, "-=0.3");
 }
 
 
 function about_text() {
-  gsap.registerPlugin(ScrollTrigger, SplitText);
+    gsap.registerPlugin(ScrollTrigger, SplitText);
 
-  const split = new SplitText(".about_me", { type: "lines" });
+    const split = new SplitText(".about_me", { type: "lines" });
 
-  split.lines.forEach((target) => {
-    gsap.to(target, {
-      backgroundPositionX: 0,
-      ease: "none",
-      scrollTrigger: {
-        trigger: target,
-        scrub: 1,
-        start: "top 100%",
-        end: "bottom bottom",
-        markers: false
-      }
+    split.lines.forEach((target) => {
+        gsap.to(target, {
+            backgroundPositionX: 0,
+            ease: "none",
+            scrollTrigger: {
+                trigger: target,
+                scrub: 1,
+                start: "top 100%",
+                end: "bottom bottom",
+                markers: false
+            }
+        });
     });
-  });
 }
 
 function horizontals() {
   gsap.registerPlugin(ScrollTrigger);
 
   const horizontalSection = document.querySelector(".horizontal");
-  const scrollLength = horizontalSection.scrollWidth - window.innerWidth;
 
   gsap.to(horizontalSection, {
-    x: -scrollLength,
+    x: () => -(horizontalSection.scrollWidth - window.innerWidth),
     ease: "none",
     scrollTrigger: {
       trigger: "#horizontal-scoll",
-      start: "top top",
-      end: () => "+=" + 2500,
+      start: "top 80px",
+      end: () => "+=" + (horizontalSection.scrollWidth - window.innerWidth),
       scrub: true,
-      pin: true,
+      pin: true,           
+      pinSpacing: true,     
       anticipatePin: 1,
-      markers: true
-    }
+      markers: false,
+    },
   });
 }
