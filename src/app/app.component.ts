a503f35d3eb41c3c02d39c66499222fc2a08e25c
Abilash Ravi <abilashravi09@gmail.com>
2026-06-10 16:52:38 +0530

    new_update_1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HeroComponent } from "./core/components/hero/hero.component";
+import { AboutComponent } from "./core/components/about/about.component";
+import { SkillsComponent } from "./core/components/skills/skills.component";
+import { CareerComponent } from "./core/components/career/career.component";
+import { HomeComponent } from "./core/home/home.component";
+import { WorksComponent } from "./core/components/works/works.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, AboutComponent, SkillsComponent, CareerComponent, HomeComponent, WorksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'portfolio';
